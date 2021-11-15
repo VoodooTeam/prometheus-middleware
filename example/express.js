3d6c5dce18f5c7b6bd58dd761d2d477ceacb74d1
wallet77 <wallet77@gmail.com>
2021-11-15 09:28:38 +0100

    doc: add express example

--- example/express.js
+++ example/express.js
@@ -4,12 +4,14 @@ const crypto = require('crypto')
 const apm = new APM()
 apm.init()
 
-const fastify = require('fastify')()
+const app = require('express')()
 
 // Declare a route
-fastify.get('/test', async (request, reply) => {
+app.get('/test', async (request, res) => {
     const salt = crypto.randomBytes(128).toString('base64')
     const hash = crypto.pbkdf2Sync('myPassword', salt, 10000, 512, 'sha512')
-    return { hash }
+    res.send({ hash })
 })
-fastify.listen(3000)
+app.listen(3000, () => {
+    console.log('Server is listening ...')
+})
