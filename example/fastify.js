0f294dde75ab847857d9afd3f2d6c2990a422568
wallet77 <wallet77@gmail.com>
2021-11-17 05:06:50 +0100

    doc: add info in fastify example

--- example/fastify.js
+++ example/fastify.js
@@ -6,10 +6,18 @@ apm.init()
 
 const fastify = require('fastify')()
 
-// Declare a route
-fastify.get('/test', async (request, reply) => {
+// Slow route
+fastify.get('/slow', async (request, reply) => {
     const salt = crypto.randomBytes(128).toString('base64')
     const hash = crypto.pbkdf2Sync('myPassword', salt, 10000, 512, 'sha512')
     return { hash }
 })
+
+// fast route
+fastify.get('/fast', async (request, reply) => {
+    return {}
+})
 fastify.listen(3000)
+
+// you can now load test this API
+// and then get metrics on http:localhost:9350/metrics
